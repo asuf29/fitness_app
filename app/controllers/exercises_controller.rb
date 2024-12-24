3d27e34343f5aa3ec0f785a7eaa948c7dadb1716
asuf29 <asuff299@gmail.com>
2024-12-24 15:37:35 +0300

    Index, show, new and form structure was created for the exercise model.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -41,7 +41,7 @@ class ExercisesController < ApplicationController
 
   private
   def exercise_params
-    params.require(:exercise).permit(:name, :content)
+    params.require(:exercise).permit(:name, :content, :image)
   end
 end
 
