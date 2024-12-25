92f8e1b5f8238cec8823114a2da8b57bf59123ce
asuf29 <asuff299@gmail.com>
2024-12-25 18:48:37 +0300

    profile page added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
+  has_one_attached :profile_picture
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
