f2201dc7b7436967211ec80c729214330dbae191
asuf29 <asuff299@gmail.com>
2024-12-25 14:45:29 +0300

    member and admin roles added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum role: { member: 0, admin: 1 }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
