fc99df356db6d988b37bf5413a2a96566c11cb5d
asuf29 <asuff299@gmail.com>
2024-12-26 10:14:25 +0300

    add edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,22 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to profile_path, notice: "Profile updated successfully."
+    else
+      flash.now[:alert] = "Failed to update profile. Please try again."
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:profile_picture, :email, :first_name, :last_name, :username)
   end
 end
