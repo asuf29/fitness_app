26763a6f64030649ae4be56b3b8329ed8211790e
asuf29 <asuff299@gmail.com>
2024-12-27 09:11:24 +0300

    Required pages for the workouts model have been added

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,51 @@
 class WorkoutsController < ApplicationController
+  before_action :set_workout, only: [:show, :edit, :update, :destroy]
+
   def index
     @workouts = current_user.workouts
+  end
+
+  def show
+  end
+
+  def new
+    @workout = current_user.workouts.new
+  end
+
+  def create
+    @workout = current_user.workouts.new(workout_params)
+    if @workout.save
+      redirect_to workouts_path, notice: "Workout created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @workout.update(workout_params)
+      redirect_to @workout, notice: "Workout updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @workout.destroy
+    redirect_to workouts_path, notice: "Workout deleted successfully!", status: :see_other
+  end
+
+  def workout_params
+    params.require(:workout).permit(:name, :c_image)
+  end
+
+  def set_workout
+    @workout = current_user.workouts.find_by(id: params[:id])
+    unless @workout
+      flash[:alert] = "Workout not found or you are not authorized to access it."
+      redirect_to workouts_path
+    end
   end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
 
-  has_one_attached :cover
+  has_one_attached :c_image
 
   validates :name, presence: true
 end
