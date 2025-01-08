243213acb772d4fd07a9367a4c83d702d1a81d47
asuf29 <asuff299@gmail.com>
2025-01-08 09:35:50 +0300

    pagy added to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
-    @articles = Article.all
+    @pagy, @articles = pagy(Article.all, limit: 5)
   end
 
   def show
