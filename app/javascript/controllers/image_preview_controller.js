0863d5d49ba8114c42fbc022fb1a50a7f8d7968d
asuf29 <asuff299@gmail.com>
2025-01-09 09:51:04 +0300

    added image preview

---
+++ app/javascript/controllers/image_preview_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['input', 'preview'];
+
+  preview() {
+    const input = this.inputTarget;
+    const preview = this.previewTarget;
+
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        preview.src = e.target.result;
+        preview.classList.remove('hidden');
+      };
+      reader.readAsDataURL(input.files[0]);
+    } else {
+      preview.classList.add('hidden');
+    }
+  }
+}
